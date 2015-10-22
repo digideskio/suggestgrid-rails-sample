0a12e9d6c061342185f108f3b6cb465ff549335e
Emrehan Tüzün <emrehan.tuzun@gmail.com>
2015-10-23 01:08:50 +0300

    Update recommenations
    Now they're showing similar items to the item shown except the item shown

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def recommend_itemids
+  def recommend_itemids(itemid)
     begin
+      # puts 'itemid is '
+      # puts itemid
+
       body = SuggestGrid::RecommendItemsBody.new
       body.userid = id.to_s
       body.size = 3
+      body.similar_itemid = itemid.to_s
+      body.except = [itemid.to_s]
 
       recommendations = SuggestGrid::RecommendationController.new.recommend_items(body, 'space', 'rating')
       recommendations['recommendations']['itemids']
