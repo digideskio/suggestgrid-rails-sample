8aeb27c86963dc82e54de3909d50bd6f98e4b7e5
Ferhat Sobay <ferhat.sobay@yahoo.com.tr>
2015-11-04 01:32:33 +0300

    catch error

--- config/initializers/suggest_grid.rb
+++ config/initializers/suggest_grid.rb
@@ -1,17 +1,17 @@
 begin
-# create StateController
-state_controller = SuggestGrid::StateController.new
+  # create StateController
+  state_controller = SuggestGrid::StateController.new
 
-# check if space exists else create it
-unless state_controller.space_exists('movie_space')['exists']
-  state_controller.create_space('movie_space')
-end
+  # check if space exists else create it
+  unless state_controller.space_exists('movie_space')['exists']
+    state_controller.create_space('movie_space')
+  end
 
-# check if type exists else create it
-unless state_controller.type_exists('movie_space','view')['exists']
-  state_controller.create_type('movie_space','view')
-end
+  # check if type exists else create it
+  unless state_controller.type_exists('movie_space','view')['exists']
+    state_controller.create_type('movie_space','view')
+  end
 
 rescue Exception => e
-  logger.error "Can not initialize SuggestGrid #{e}"
+  Rails.logger.error "Can not initialize SuggestGrid #{e}"
 end
