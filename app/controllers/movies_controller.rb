86ea50bc054e306215be416c7730df746efb5851
ferhatsb <ferhat.sobay@yahoo.com.tr>
2016-06-06 11:46:53 +0300

    clean up

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,24 @@
 class MoviesController < ApplicationController
 
   def index
-    @recommended_movies = Movie.recommend(current_or_guest_user, 4)
-    @all_movies = Movie.all
+    @movies = {
+        recommended: Movie.recommend(current_or_guest_user, 4),
+        all: Movie.all
+    }
   end
 
   def show
-    @movie = Movie.find(params[:id])
-    @movie.create_action(current_or_guest_user)
+
+    movie = Movie.find(params[:id])
+
+    # send view information to SuggestGrid
+    movie.create_action(current_or_guest_user)
+
+    @movie = {
+        movie: movie,
+        recommended_similar: movie.recommend_similar(current_or_guest_user, 12),
+        similar: movie.similar(5)
+    }
   end
 
 end
